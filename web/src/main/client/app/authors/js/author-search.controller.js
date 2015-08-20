1ecae6238830b2ce68ae6aae28f3b5aae6787235
Marta Kordulska <marta.kordulska@capgemini.com>
2015-08-20 17:14:39 +0200

    new dialog Author List added

---
+++ web/src/main/client/app/authors/js/author-search.controller.js
@@ -0,0 +1,15 @@
+angular.module('app.authors').controller('AuthorSearchController', function ($scope, $window, $location, authorService, Flash) {
+    'use strict';
+
+    $scope.authors = [];
+    $scope.gridOptions = { data: 'authors' };
+
+    $scope.search = function () {
+        authorService.search().then(function (response) {
+            angular.copy(response.data, $scope.authors);
+        }, function () {
+            Flash.create('danger', 'Wyjątek', 'custom-class');
+        });
+    };
+
+});
